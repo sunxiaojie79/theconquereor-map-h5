734d7eb42f078023b3bb8514ce4e3ea31f189c35
sunxiaojie79 <297280911@qq.com>
2025-07-11 00:03:46 +0800

    feat: px to vw

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,20 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-const autoprefixer = require("autoprefixer");
-const px2viewport = require("postcss-px-to-viewport-8-plugin");
+import autoprefixer from "autoprefixer";
+import px2viewport from "postcss-px-to-viewport-8-plugin";
 
-module.exports = {
+export default {
   plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    autoprefixer(),
     px2viewport({
-      viewportWidth: 375,
-      exclude: [/node_modules/],
+      viewportWidth: 375, // 设计稿宽度
+      viewportHeight: 667, // 设计稿高度，可选
+      unitPrecision: 3, // 转换后的精度，即小数点位数
+      viewportUnit: "vw", // 指定需要转换成的视口单位
+      selectorBlackList: [".ignore", ".hairlines"], // 指定不转换为视口单位的类
+      minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+      mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
+      replace: true, // 是否转换后直接更换属性值
+      exclude: [/node_modules/], // 设置忽略文件，用正则做目录名匹配
+      landscape: false, // 是否处理横屏情况
     }),
+    autoprefixer(),
   ],
 };
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 import Components from "unplugin-vue-components/vite";
 import { VantResolver } from "unplugin-vue-components/resolvers";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
     Components({
       resolvers: [VantResolver()],
     }),
